736201cef027040936f50a048104a23161463397
terranceblandford <terranceblandford@gmail.com>
2025-10-18 15:21:02 -0700

    loops and functions

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-# example workign with Loops
+# example workign with Loops and Functions
 #By Terrance Blandford
 
-question = input("Is today a good day? Please answer with y or n: ")
+def send_message():
+    number = 1
+    while number <= 10:
+        print ("Yeah it is")
+        number += 1
+
+question = input("Is today a good day? Please answer with y or n: ").strip().lower()
 
 if question == "y" :
-    #Create a loop of 10 times saying yea it is"
-    number = 1
-    while number < 11:
-        print("Yes it is")
-        number += 1
+   send_message()
 elif question == 'n' :
     print("Thats ok tomorrow will be!")
 else:
