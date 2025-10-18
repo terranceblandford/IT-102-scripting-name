4cd0416166f331ec7ec4d4ae31f1395f3cc91777
terranceblandford <terranceblandford@gmail.com>
2025-10-18 14:58:34 -0700

    read file

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By Terrance Blandford
+
+print ("Here is someone to hack information")
+with open("hackme.txt", "r") as file:
+    contents = file.read()
+    
+print("The information found in hackme.txt is: \n")
+print(contents)
